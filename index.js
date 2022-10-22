11b8610ed389ee17ce928538ca1a9d70738f6fe8
syed abdul hakim <s.abdulhakim10@gmail.com>
2022-10-23 01:06:24 +0600

    create news and category data loading API

--- index.js
+++ index.js
@@ -7,12 +7,33 @@ app.use(cors());
 
 const categories = require('./data/categories.json');
 
+const news = require('./data/news.json');
+
 app.get('/', (req, res) => {
     res.send('News API Running...');
 })
 
 app.get('/news-categories', (req, res) => {
     res.send(categories);
+})
+
+// news load by category_id
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if(id === '08'){
+        res.send(news);
+    }
+    else{
+        const categoryNews = news.filter(n => n.category_id === id);
+        res.send(categoryNews);
+    }
+})
+
+// each news load by news id
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
 })
 
 app.listen(port, () => {
